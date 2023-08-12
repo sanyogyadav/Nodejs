87a8a49bbf0dd56e851d0229a261f193f881c201
sanyogyadav <sanyogyadav52@gmail.com>
2023-08-12 15:18:55 +0530

    [SANYOG]- Services created for API's.

---
+++ src/app/services/crud.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  constructor(private http: HttpClient) { }
+
+  url = 'http://localhost:3000/api/students';
+
+  //Add student API method POST
+  createStudents(id: number, name: string, email: string, age: number, city: string): Observable<any> {
+    return this.http.post(`${this.url}`, {
+      id: id,
+      name: name,
+      email: email,
+      age: age,
+      city: city
+    })
+  }
+
+  //get All students list API method GET
+  getAllStudents():Observable<any[]> {
+    return this.http.get<any[]>(`${this.url}/all`);
+  }
+
+  //get student by ID API method GET
+  getAllStudentsById(id: number):Observable<any[]> {
+    return this.http.get<any[]>(`${this.url}/all/${id}`);
+  }
+
+  //update student by ID API method PUT
+  updateStudentById(id: number, name: string, email: string, age: number, city: string):Observable<any> {
+    return this.http.put<any>(`${this.url}/${id}`, {
+      name: name,
+      email: email,
+      age: age,
+      city: city
+    });
+  }
+
+  //delete student by ID API method delete
+  deleteStudentById(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.url}/${id}`);
+  }
+
+  //delete all students API method delete
+  deleteAllStudent(): Observable<any> {
+    return this.http.delete<any>(`${this.url}`);
+  }
+
+  //search student by name API method GET
+  searchStudentByName(): Observable<any> {
+    return this.http.get<any>(`${this.url}/?`);
+  }
+
+}
